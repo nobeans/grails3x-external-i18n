6f0d6e20efec238946b8923012c3892871469f25
Yasuharu Nakano <ynak@jggug.org>
2015-11-05 15:03:19 +0900

    Support external i18n messages for Grails 3.x

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,30 @@
+import org.grails.spring.context.support.ReloadableResourceBundleMessageSource
+import grails.util.Holders
+
 class BootStrap {
 
+    def messageSource
+
     def init = { servletContext ->
+        setupAdditionalMessageSources(messageSource)
+        println "messageSource: " + messageSource.dump() // for DEBUG
     }
+
     def destroy = {
+    }
+
+    // Grails 3.xではPluginAwareResourceBundleMessageSource#afterPropertiesSet()がbasenamesを自動再構築してしまうため、
+    // このようにビーンの初期化後のBootStrapなどのタイミングで、設定を反映する必要がある。
+    private setupAdditionalMessageSources(messageSource) {
+        // 追加ファイル内にエントリが見つからなかったキーは、最終的にGrailsが自動的にセットアップする標準方法で解決する。
+        // このためにprivateフィールドになっているオリジナル設定を強引ではあるが取得しておく。
+        List originalBasenames = ReloadableResourceBundleMessageSource.getDeclaredField("basenames").with { field ->
+            field.accessible = true
+            field.get(messageSource)
+        }
+
+        // 設定を変更する。
+        messageSource.basenames = Holders.config.i18n.messageSource.additionalBasenames + originalBasenames
+        messageSource.defaultEncoding = Holders.config.i18n.messageSource.defaultEncoding
     }
 }
